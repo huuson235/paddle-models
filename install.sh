7a3b130b5a8981a38fc1f9afd26699bf067b942d
SonNHH <sonnhh@vps.com.vn>
2024-08-20 08:55:47 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.paddleocr/whl/det/en  ~/.paddleocr/whl/rec/en ~/.paddleocr/whl/cls
+tar -xvf en_PP-OCRv3_det_infer.tar -C ~/.paddleocr/whl/det/en
+tar -xvf en_PP-OCRv4_rec_infer.tar -C ~/.paddleocr/whl/rec/en
+tar -xvf ch_ppocr_mobile_v2.0_cls_infer.tar -C ~/.paddleocr/whl/cls
+
